789d84936d7ddcfc94bd35c001f799c49e148a95
刘琰 <liuy@huadi.info>
2021-01-20 09:55:23 +0800

    goods

--- routers/goods-router.js
+++ routers/goods-router.js
@@ -2,6 +2,9 @@ const Router = require("@koa/router")
 const GoodsGatetory = require("../models/goods-category-model")
 const Goods = require("../models/goods-model")
 const GoodsInfo = require("../models/goods-info-model")
+const GoodsSku = require("../models/goods-sku-model")
+const GoodsAttrKey = require("../models/goods-attr-key-model")
+const GoodsAttrValue = require("../models/goods-attr-value-model")
 
 const router = new Router({
     prefix:"/goods"
@@ -53,4 +56,56 @@ router.get("/goods",async function (ctx){
         data: goods
     }
 })
+
+router.get("/goods/:id",async function (ctx){
+    let goodsId = Number(ctx.params.id)
+    Goods.hasMany(GoodsInfo,{foreignKey:'goods_id',targetKey:'id'})
+    let goods = await Goods.findOne({
+        where: {
+            id:goodsId
+        },
+        include:[{
+            model:GoodsInfo,
+            attributes:['content','kind','goods_id']
+        }]
+    })
+    ctx.status = 200
+    ctx.body = {
+        code: 200,
+        msg: 'ok',
+        data: goods
+    }
+})
+
+router.get("/goods/:id/sku",async function (ctx){
+    let goodsId = Number(ctx.params.id)
+    GoodsAttrKey.hasMany(GoodsAttrValue,{foreignKey:'attr_key_id',targetKey:'id'})
+
+    let goodsSku = await GoodsSku.findAll({
+        where: {
+            goods_id:goodsId
+        }
+    })
+    let goodsAttrKeys = await GoodsAttrKey.findAll({
+        where:{
+            goods_id:goodsId
+        },
+        attributes:['id','attr_key','goods_id'],
+        include:[{
+            model:GoodsAttrValue,
+            attributes:['id','attr_key_id','attr_value','goods_id']
+        }],
+        distinct:true
+    })
+    ctx.status = 200
+    ctx.body = {
+        code: 200,
+        msg: 'ok',
+        data: {
+            goodsSku,
+            goodsAttrKeys
+        }
+    }
+})
+
 module.exports = router
